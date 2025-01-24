9a6add6b242bc653872b8cca0526222767281b35
Davi <dp.junior1997@gmail.com>
2025-01-24 16:01:17 -0300

    feat: adding Inicio component

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+const Main = styled.main``;
+
+const MainTextoContainer = styled.div``;
+
+const MainFoto = styled.img``;
+
 const Inicio = () => {
-  return <></>;
+  return (
+    <Main>
+      <MainTextoContainer>
+        <h1>Davi Pereira</h1>
+        <h4>Full Stack Developer</h4>
+        <p>
+          Bem-vindo ao meu portfólio! Explore meus projetos e conheça mais sobre
+          mim.
+        </p>
+      </MainTextoContainer>
+
+      <MainFoto></MainFoto>
+    </Main>
+  );
 };
 
 export default Inicio;
