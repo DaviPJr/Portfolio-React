7e9cec993b872336962b69b99f68bc7a2b5e9c4c
Davi <dp.junior1997@gmail.com>
2025-01-29 17:24:06 -0300

    feat: adding SobreMim page

--- src/pages/SobreMim.jsx
+++ src/pages/SobreMim.jsx
@@ -1,7 +1,70 @@
 import styled from "styled-components";
 
+import foto from "../assets/ft2.jpeg";
+
+const MainEstilizado = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1000px;
+  gap: 100px;
+`;
+
+const InfosContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  width: 35%;
+`;
+
+const TituloEstilizado = styled.h1`
+  color: white;
+`;
+
+const ParagrafoEstilizado = styled.p`
+  color: #d1d5db;
+`;
+
+const FotoEstilizada = styled.img`
+  height: 500px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.5);
+`;
+
 const SobreMim = () => {
-  return <main></main>;
+  return (
+    <MainEstilizado>
+      <div>
+        <FotoEstilizada src={foto} alt="Foto de Davi Pereira" />
+      </div>
+      <InfosContainer>
+        <TituloEstilizado>Davi Pereira</TituloEstilizado>
+        <ParagrafoEstilizado>
+          Cursando Análise e Desenvolvimento de Sistemas.
+        </ParagrafoEstilizado>
+        <ParagrafoEstilizado>
+          Sou um desenvolvedor web fullstack, apaixonado por tecnologia e
+          inovação. Acredito que a tecnologia tem o poder de transformar ideias
+          em soluções que impactam positivamente a vida das pessoas.
+        </ParagrafoEstilizado>
+        <ParagrafoEstilizado>
+          Sou uma pessoa curiosa por natureza e sempre em busca de aprendizado.
+          Tenho experiência no desenvolvimento de aplicações completas e
+          robustas, com foco em designs atraentes que proporcionam uma
+          experiência de usuário excepcional.
+        </ParagrafoEstilizado>
+        <ParagrafoEstilizado>
+          Atualmente, estou aprimorando minhas habilidades em ferramentas
+          modernas como React, Next.js, React Native, entre outras. Busco
+          oportunidades para colaborar com times alinhados aos meus valores, em
+          um ambiente que promova crescimento profissional e desafios
+          estimulantes. Meu objetivo é contribuir com projetos complexos e
+          inovadores, onde eu possa aplicar meus conhecimentos e continuar
+          evoluindo como profissional.
+        </ParagrafoEstilizado>
+      </InfosContainer>
+    </MainEstilizado>
+  );
 };
 
 export default SobreMim;
